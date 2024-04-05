66f67edd5a588ed80cedbaa9398140373c2e9147
fajarudinzakariya <fajarudinzakariyaa8@gmail.com>
2024-04-06 00:46:59 +0700

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,11 +64,21 @@ const Header = () => {
           alignItems="center"
         >
           <nav>
-            {/* Add social media links based on the `socials` data */}
+            {
+              <HStack spacing={5}>
+                {
+                  socials.map((social) => <a href={social.url} key={social.url}
+                  >
+                    <FontAwesomeIcon icon={social.icon} size="2x" />
+                  </a>)
+                }
+
+              </HStack>}
           </nav>
           <nav>
             <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
+              <a href="#projects-section" onClick={handleClick("projects")}>Projects</a>
+              <a href="#contactme-section" onClick={handleClick("contactme")}>Contact Me</a>
             </HStack>
           </nav>
         </HStack>
