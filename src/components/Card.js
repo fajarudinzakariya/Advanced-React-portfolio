fc82c1418fa4276f2a47897a05536acdb579609c
fajarudinzakariya <fajarudinzakariyaa8@gmail.com>
2024-04-06 02:38:44 +0700

    add image

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,12 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack>
+      <Image src={imageSrc} borderRadius='xl' />
+    </VStack>
+  );
+
 };
 
 export default Card;
